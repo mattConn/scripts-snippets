31671e65c6658c68f03293154f1235727278fbfd
Matthew Connelly <mattconndev@gmail.com>
2018-01-16 20:21:10 -0500

    addition of vanilla js keybinding snippet

---
+++ vanilla_keybinding.js
@@ -0,0 +1,25 @@
+document.onkeydown = keyCheck;
+
+function keyCheck(e)
+{
+  var keycode = window.event.keyCode;
+
+  switch (keycode)
+  {
+    case 37:  // left arrow 
+		// action
+	break;
+
+    case 38:  // up arrow 
+		// action
+	break;
+
+    case 39:  // right arrow 
+		// action
+	break;
+
+    case 40:  // down arrow 
+		// action
+	break;
+  }
+}
