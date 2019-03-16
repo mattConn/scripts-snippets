8309c78dfe50738525dd699da420c00905ce09fa
Matthew Connelly <matthew.b.connelly@gmail.com>
2019-03-15 20:15:28 -0400

    added error printing macro

---
+++ error-printing.cpp
@@ -0,0 +1,8 @@
+// debug msg printing
+#define DEBUG
+
+#ifdef DEBUG
+#define DEBUG_MSG(str) do { std::cout << str << std::endl; } while( false )
+#else
+#define DEBUG_MSG(str) do { } while ( false )
+#endif
