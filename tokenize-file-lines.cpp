f18dc8aad8c5fba8c84851a9c6733f67591f31f0
Matthew Connelly <matthew.b.connelly@gmail.com>
2019-09-10 21:17:29 -0400

    updated token file lines cpp

--- tokenize-file-lines.cpp
+++ tokenize-file-lines.cpp
@@ -1,12 +1,28 @@
-// tokenize file
-string line; // file line buffer
-string tok; // line token buffer
-while(getline(infile, line)) // newline delimiter
-{
-	stringstream ss(line); // stringstream from line
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <fstream>
 
-	while(getline(ss, tok, ' ')) // space delimiter
-	{
-		// token handled here
-	}
+
+int main()
+{
+
+    std::ifstream infile("test.txt");
+
+    // tokenize file
+    std::string line; // file line buffer
+    std::string tok; // line token buffer
+
+    while(std::getline(infile, line)) // newline delimiter
+    {   
+        std::stringstream ss(line); // stringstream from line
+        
+        while(std::getline(ss, tok, ' ')) // space delimiter
+        {   
+            std::cout << tok << std::endl;
+            // token handled here
+        }
+    }
+    return 0;
 }
+
